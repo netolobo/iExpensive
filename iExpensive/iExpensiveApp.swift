cfb6e5a09c1b379d6f00bf241c4633edfdca2d0a
netolobo <desilio@gmail.com>
2023-10-18 13:16:30 -0700

    Initial Commit

---
+++ iExpensive/iExpensiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensiveApp.swift
+//  iExpensive
+//
+//  Created by Neto Lobo on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpensiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
